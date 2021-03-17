8c7bf940fa93aa3e99dea0791f4f888869438fe5
Tal Safran <talsafran@github.com>
2021-03-17 17:56:54 +0200

    Add files from lecture

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const quantitySelector = document.getElementById('quantity-selector')
+
+  quantitySelector.addEventListener('change', (event) => {
+    const numberOfServings = event.target.value
+    const quantityElements = document.getElementsByClassName('quantity')
+
+    for (const quantityElement of quantityElements) {
+      const quantityPerServing = quantityElement.getAttribute('data-quantity-per-serving')
+
+      quantityElement.innerHTML = numberOfServings * quantityPerServing
+    }
+  })
+})
